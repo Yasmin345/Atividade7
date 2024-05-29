a61bc53e7325636ad823e3f65f61f5683b394a89
Yasmin345 <Yasminmonteiros29@gmail.com>
2024-05-29 10:02:21 -0300

    [feat] - adicionndo atributos e metodos da classe Eevee

---
+++ Polimorfismo.java/src/Eevee.java
@@ -0,0 +1,30 @@
+public class Eevee {
+    // declarando atributos
+
+    String tipo;
+    double peso;
+    int altura; 
+    int hp;
+
+
+    // métodos
+
+    public String Ataque(){
+        return "* Ataque Rapido *";
+    }
+
+
+    public String Defesa(){
+        return "* Desvio *";
+    }
+
+
+        public String Especial(){
+            return "* Tri-Ataque *";
+        }
+      
+
+
+
+
+}
